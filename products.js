be943c38044364da5fa08117bc71b1f23e14ec10
Brevan Bender <bcaybender@gmail.com>
2021-10-11 13:29:00 -0700

    added some styles and set up html

---
+++ products.js
@@ -0,0 +1,37 @@
+export const pokegoods = [
+    {
+    id:'1', 
+    name: 'potion', 
+    img: '', 
+    price: '', 
+    description: '',
+},
+    {
+    id: '2',  
+    name: 'potion', 
+    img: '', 
+    price: '', 
+    description: ''
+},
+    {
+    id: '3', 
+    name: 'potion', 
+    img: '',
+    price: '', 
+    description: '',
+},
+    {
+    id: '4',  
+    name: 'potion', 
+    img: '', 
+    price: '',
+    description: '',
+    },
+    {
+     id: '5',   
+    name: 'potion',
+    img: '', 
+    price: '', 
+    description: '',
+    },
+];
