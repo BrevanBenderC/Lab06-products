cf3beb9c9749fa85dcfd8f6a8416889d3b623ed6
Brevan Bender <bcaybender@gmail.com>
2021-10-11 14:05:23 -0700

    Finished the list of products

--- render-product.js
+++ render-product.js
@@ -1,3 +1,13 @@
 export function renderMe (pokegoodsObject){
+    const itemCard = document.createElement('div');
+    itemCard.classList.add('item-card');
     
+    const itemHeader = document.createElement('h2');
+    itemHeader.textContent = item.name;
+    
+    const img = document.createElement('img');
+    img.src = item.img;
+    
+    itemCard.append(itemHeader, img);
+    return itemCard;
 }
